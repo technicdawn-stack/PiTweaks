70a3bf74fa4ce758332b07947794f46e82376eb2
technicdawn-stack <connorbelfield@hotmail.com>
2026-08-22 21:49:44 +0100

    Update conspy_control.sh 1.1

--- conspy_control.sh
+++ conspy_control.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Description: Standalone Conspy screen mirroring controller and setup manager
+# Description: Standalone Conspy screen mirroring controller and setup manager v1.1
 
 # Check for whiptail (install if missing)
 if ! command -v whiptail &> /dev/null; then
