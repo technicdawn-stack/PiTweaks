755d2346fa25cc19d6cf575292d1363ea2e73901
technicdawn-stack <connorbelfield@hotmail.com>
2026-08-23 22:58:04 +0100

    Update A_Test.sh

--- A_Test.sh
+++ A_Test.sh
@@ -1,9 +1,7 @@
 # Description: Loops the install url for faster and easier refreshing.
 # PERSISTENT: FALSE
 
-while true; do
-    clear
-    bash <(curl -fsSL "https://raw.githubusercontent.com/technicdawn-stack/PiTweaks/main/install.sh")
-    echo ""
-    read -p "Press [Enter] to fetch the latest cache and re-run..."
-done
+clear
+bash <(curl -fsSL "https://raw.githubusercontent.com/technicdawn-stack/PiTweaks/main/install.sh")
+echo ""
+
