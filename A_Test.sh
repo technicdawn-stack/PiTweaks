edf188d0c19f95f37c90256e2d91db740fae234c
technicdawn-stack <connorbelfield@hotmail.com>
2026-08-28 19:25:25 +0100

    Added # Category:

--- A_Test.sh
+++ A_Test.sh
@@ -1,5 +1,6 @@
 # Description: Loops the install url for faster and easier refreshing. V1.1
 # PERSISTENT: FALSE
+# Category: Tools
 
 clear
 bash <(curl -fsSL "https://raw.githubusercontent.com/technicdawn-stack/PiTweaks/main/install.sh")
